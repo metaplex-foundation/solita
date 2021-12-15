10d191d6c86eb91657383fdd7ddcc33426cddac2
Thorsten Lorenz <thlorenz@gmx.de>
2021-12-15 08:36:02 -0500

    errors: adding lookup functions

--- src/render-errors.ts
+++ src/render-errors.ts
@@ -12,15 +12,32 @@ export class ${className} extends Error {
   readonly code: number = ${code};
   readonly name: string = '${name}';
   constructor() {
-    super('${msg}');
+    super('${msg.replace(/[']/g, `\\'`)}');
     Error.captureStackTrace(this, ${className});
   }
 }
+
+createErrorFromCodeLookup.set(${code}, () => new ${className}())
+createErrorFromNameLookup.set('${name}', () => new ${className}())
 `
 }
 
 export function renderErrors(errors: AnchorError[]) {
-  return errors.map(renderError).join('\n')
+  const errorsCode = errors.map(renderError).join('\n')
+  return `const createErrorFromCodeLookup: Map<number, () => Error> = new Map();
+const createErrorFromNameLookup: Map<string, () => Error> = new Map();
+${errorsCode}
+
+export function errorFromCode(code: number): Error | null {
+  const createError = createErrorFromCodeLookup.get(code)
+  return createError == null ? createError() : null;
+}
+
+export function errorFromName(name: string): Error | null {
+  const createError = createErrorFromNameLookup.get(name)
+  return createError == null ? createError() : null;
+}
+`
 }
 
 if (module === require.main) {
