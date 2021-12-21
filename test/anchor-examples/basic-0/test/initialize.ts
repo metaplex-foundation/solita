cc36d702bf5387bb247b01cd89f5bdf565ec0566
Thorsten Lorenz <thlorenz@gmx.de>
2021-12-21 12:20:37 -0500

    basic0: proper tests

--- test/anchor-examples/basic-0/test/initialize.ts
+++ test/anchor-examples/basic-0/test/initialize.ts
@@ -1,18 +1,27 @@
-import { Connection, Keypair, PublicKey, Transaction } from '@solana/web3.js'
+import test from 'tape'
+import { Connection, Transaction } from '@solana/web3.js'
 import { createInitializeInstruction } from '../src/'
 import {
+  AddressLabels,
   airdrop,
+  assertConfirmedTransaction,
+  assertTransactionSummary,
   LOCALHOST,
   PayerTransactionHandler,
 } from '@metaplex-foundation/amman'
 
-function genKeypair(): [PublicKey, Keypair] {
-  const kp = Keypair.generate()
-  return [kp.publicKey, kp]
-}
+import idl from '../target/idl/basic_0.json'
+;(function killStuckProcess() {
+  test.onFinish(() => process.exit(0))
+})()
 
-async function main() {
-  const [payer, payerKeypair] = genKeypair()
+const addressLabels = new AddressLabels(
+  { basic0: idl.metadata.address },
+  console.log
+)
+
+test('initialize', async (t) => {
+  const [payer, payerKeypair] = addressLabels.genKeypair('payer')
   const connection = new Connection(LOCALHOST, 'confirmed')
   const transactionHandler = new PayerTransactionHandler(
     connection,
@@ -26,8 +35,9 @@ async function main() {
   const res = await transactionHandler.sendAndConfirmTransaction(tx, [
     payerKeypair,
   ])
-  console.log(res)
-}
 
-console.log('Running client.')
-main().then(() => console.log('Success'))
+  assertConfirmedTransaction(t, res.txConfirmed)
+  assertTransactionSummary(t, res.txSummary, {
+    msgRx: [/instruction: initialize/i, /success/],
+  })
+})
