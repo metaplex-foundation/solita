39866c6fb56b8c73b161d1057139492235d18a34
Thorsten Lorenz <thlorenz@gmx.de>
2021-12-27 15:56:37 -0600

    test: adding anchor CPI tutorial tests (#10)
    * chore: update amman
    
    * test: adding anchor CPI test

---
+++ test/anchor-examples/basic-3/scripts/api-gen.js
@@ -0,0 +1,54 @@
+// @ts-check
+'use strict'
+
+const path = require('path')
+const { spawn } = require('child_process')
+const { SolanaIdlToApi } = require('../../../../dist/src/solana-idl-to-api')
+const { writeFile } = require('fs/promises')
+
+const generatedIdlDir = path.join(__dirname, '..', 'idl')
+const anchor = spawn('anchor', ['build', '--idl', generatedIdlDir])
+  .on('error', (err) => {
+    console.error(err)
+    // @ts-ignore this err does have a code
+    if (err.code === 'ENOENT') {
+      console.error(
+        'Ensure that `anchor` is installed and in your path, see:\n  https://project-serum.github.io/anchor/getting-started/installation.html#install-anchor\n'
+      )
+    }
+    process.exit(1)
+  })
+  .on('exit', async () => {
+    console.error('IDLs written to: %s', generatedIdlDir)
+    await generateTypeScriptSDK({
+      programName: 'puppet',
+      programId: 'Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS',
+    })
+    await generateTypeScriptSDK({
+      programName: 'puppet_master',
+      programId: 'HmbTLCmaGvZhKnn1Zfa1JVnp7vkMV4DYVxPLWBVoN65L',
+    })
+  })
+
+anchor.stdout.on('data', (buf) => console.log(buf.toString('utf8')))
+anchor.stderr.on('data', (buf) => console.error(buf.toString('utf8')))
+
+async function generateTypeScriptSDK({ programName, programId }) {
+  const generatedSDKDir = path.join(
+    __dirname,
+    '..',
+    'src',
+    'generated',
+    programName
+  )
+  console.error('Generating TypeScript SDK to %s', generatedSDKDir)
+  const generatedIdlPath = path.join(generatedIdlDir, `${programName}.json`)
+
+  const idl = require(generatedIdlPath)
+  if (idl.metadata?.address == null) {
+    idl.metadata = { ...idl.metadata, address: programId }
+    await writeFile(generatedIdlPath, JSON.stringify(idl, null, 2))
+  }
+  const gen = new SolanaIdlToApi(idl, { formatCode: true })
+  return gen.renderAndWriteTo(generatedSDKDir)
+}
---
+++ test/anchor-examples/basic-3/src/index.ts
@@ -0,0 +1,3 @@
+export * from './generated/puppet/accounts'
+export * from './generated/puppet/instructions'
+export * from './generated/puppet_master/instructions'
