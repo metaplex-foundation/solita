5f75c9cbede7f46d2f4b4fce586b5666233d5c4e
Thorsten Lorenz <thlorenz@gmx.de>
2022-01-09 16:01:54 -0600

    type: typemapper renderes serde functions

--- src/type-mapper.ts
+++ src/type-mapper.ts
@@ -25,6 +25,7 @@ import {
   assertKnownSerdePackage,
   SerdePackage,
   serdePackageExportName,
+  serdePackageTypePrefix,
 } from './serdes'
 
 export function resolveSerdeAlias(ty: string) {
@@ -98,6 +99,9 @@ export class TypeMapper {
     return { typescriptType, pack, sourcePack }
   }
 
+  // -----------------
+  // Map TypeScript Type
+  // -----------------
   private mapPrimitiveType(ty: IdlType & string, name: string) {
     this.assertBeetSupported(ty, 'map primitive type')
     const mapped = this.primaryTypeMap[ty]
@@ -116,7 +120,7 @@ export class TypeMapper {
     return typescriptType
   }
 
-  private mapOption(ty: IdlTypeOption, name: string) {
+  private mapOptionType(ty: IdlTypeOption, name: string) {
     const inner = this.map(ty.option, name)
     const optionPackage = BEET_PACKAGE
     this.serdePackagesUsed.add(optionPackage)
@@ -124,12 +128,12 @@ export class TypeMapper {
     return `${exp}.COption<${inner}>`
   }
 
-  private mapVec(ty: IdlTypeVec, name: string) {
+  private mapVecType(ty: IdlTypeVec, name: string) {
     const inner = this.map(ty.vec, name)
     return `${inner}[]`
   }
 
-  private mapDefined(ty: IdlTypeDefined) {
+  private mapDefinedType(ty: IdlTypeDefined) {
     const userDefinedPackage = LOCAL_TYPES_PACKAGE
     this.serdePackagesUsed.add(userDefinedPackage)
     const exp = serdePackageExportName(userDefinedPackage)
@@ -141,15 +145,69 @@ export class TypeMapper {
       return this.mapPrimitiveType(ty, name)
     }
     if (isIdlTypeOption(ty)) {
-      return this.mapOption(ty, name)
+      return this.mapOptionType(ty, name)
     }
     if (isIdlTypeVec(ty)) {
-      return this.mapVec(ty, name)
+      return this.mapVecType(ty, name)
     }
     if (isIdlTypeDefined(ty)) {
-      return this.mapDefined(ty)
+      return this.mapDefinedType(ty)
     }
-    return 'FAIL'
+
+    throw new Error(`Type ${ty} required for ${name} is not yet supported`)
+  }
+
+  // -----------------
+  // Map Serde
+  // -----------------
+  private mapPrimitiveSerde(ty: IdlType & string, name: string) {
+    this.assertBeetSupported(ty, `account field ${name}`)
+    const mapped = this.primaryTypeMap[ty]
+
+    assertKnownSerdePackage(mapped.sourcePack)
+    const packExportName = serdePackageExportName(mapped.sourcePack)
+    this.serdePackagesUsed.add(mapped.sourcePack)
+    return `${packExportName}.${ty}`
+  }
+
+  private mapOptionSerde(ty: IdlTypeOption, name: string) {
+    const inner = this.mapSerde(ty.option, name)
+    const optionPackage = BEET_PACKAGE
+    this.serdePackagesUsed.add(optionPackage)
+    const exp = serdePackageExportName(optionPackage)
+    return `${exp}.coption(${inner})`
+  }
+
+  private mapVecSerde(ty: IdlTypeVec, name: string) {
+    const inner = this.mapSerde(ty.vec, name)
+    const fixedSizeArrayPackage = BEET_PACKAGE
+    this.serdePackagesUsed.add(fixedSizeArrayPackage)
+    const exp = serdePackageExportName(fixedSizeArrayPackage)
+    // TODO(thlorenz): for now hardcoding `1` until we figure out how to handle dynamically sized arrays
+    return `${exp}.fixedSizeArray(${inner}, 1)`
+  }
+
+  private mapDefinedSerde(ty: IdlTypeDefined) {
+    const userDefinedPackage = LOCAL_TYPES_PACKAGE
+    this.serdePackagesUsed.add(userDefinedPackage)
+    const exp = serdePackageExportName(userDefinedPackage)
+    return `${exp}.${ty.defined}.struct`
+  }
+
+  mapSerde(ty: IdlType, name: string = '<no name provided>'): string {
+    if (typeof ty === 'string') {
+      return this.mapPrimitiveSerde(ty, name)
+    }
+    if (isIdlTypeOption(ty)) {
+      return this.mapOptionSerde(ty, name)
+    }
+    if (isIdlTypeVec(ty)) {
+      return this.mapVecSerde(ty, name)
+    }
+    if (isIdlTypeDefined(ty)) {
+      return this.mapDefinedSerde(ty)
+    }
+    throw new Error(`Type ${ty} required for ${name} is not yet supported`)
   }
 
   assertBeetSupported(
--- test/type-mapper.ts
+++ test/type-mapper.ts
@@ -3,6 +3,7 @@ import spok from 'spok'
 import { TypeMapper } from '../src/type-mapper'
 import {
   BEET_PACKAGE,
+  BEET_SOLANA_PACKAGE,
   IdlType,
   LOCAL_TYPES_PACKAGE,
   SOLANA_WEB3_PACKAGE,
@@ -14,19 +15,41 @@ import {
 test('type-mapper: primitive types - numbers', (t) => {
   const tm = new TypeMapper()
 
-  for (const n of <IdlType[]>['i8', 'u32', 'i16']) {
+  const types = <IdlType[]>['i8', 'u32', 'i16']
+  for (const n of types) {
     const ty = tm.map(n)
     t.equal(ty, 'number', `'${n}' maps to '${ty}' TypeScript type`)
   }
+
+  tm.clearSerdePackagesUsed()
+  for (const n of types) {
+    const serde = tm.mapSerde(n)
+    t.equal(serde, `beet.${n}`, `'${n}' maps to '${serde}' serde`)
+  }
+  spok(t, Array.from(tm.serdePackagesUsed), {
+    $topic: 'serdePackagesUsed',
+    ...[BEET_PACKAGE],
+  })
   t.end()
 })
 
 test('type-mapper: primitive types - bignums', (t) => {
   const tm = new TypeMapper()
+  const types = <IdlType[]>['i64', 'u128', 'i256', 'u512']
 
-  for (const n of <IdlType[]>['i64', 'u128', 'i256', 'u512']) {
+  for (const n of types) {
     const ty = tm.map(n)
     t.equal(ty, 'beet.bignum', `'${n}' maps to '${ty}' TypeScript type`)
+  }
+  spok(t, Array.from(tm.serdePackagesUsed), {
+    $topic: 'serdePackagesUsed',
+    ...[BEET_PACKAGE],
+  })
+
+  tm.clearSerdePackagesUsed()
+  for (const n of types) {
+    const serde = tm.mapSerde(n)
+    t.equal(serde, `beet.${n}`, `'${n}' maps to '${serde}' serde`)
   }
   spok(t, Array.from(tm.serdePackagesUsed), {
     $topic: 'serdePackagesUsed',
@@ -42,11 +65,21 @@ test('type-mapper: composite types - option<number | bignum>', (t) => {
   const tm = new TypeMapper()
 
   {
-    const ty = tm.map({
+    const type = <IdlType>{
       option: 'u16',
-    })
+    }
+    const ty = tm.map(type)
 
     t.equal(ty, 'beet.COption<number>', 'option<u16>')
+    spok(t, Array.from(tm.serdePackagesUsed), {
+      $topic: 'serdePackagesUsed',
+      ...[BEET_PACKAGE],
+    })
+
+    tm.clearSerdePackagesUsed()
+    const serde = tm.mapSerde(type)
+    t.equal(serde, 'beet.coption(beet.u16)', 'option<u16> serde')
+
     spok(t, Array.from(tm.serdePackagesUsed), {
       $topic: 'serdePackagesUsed',
       ...[BEET_PACKAGE],
@@ -54,15 +87,28 @@ test('type-mapper: composite types - option<number | bignum>', (t) => {
   }
 
   {
-    const ty = tm.map({
+    tm.clearSerdePackagesUsed()
+    const type = <IdlType>{
       option: 'u64',
-    })
+    }
+
+    const ty = tm.map(type)
     t.equal(ty, 'beet.COption<beet.bignum>', 'option<u64>')
     spok(t, Array.from(tm.serdePackagesUsed), {
       $topic: 'serdePackagesUsed',
       ...[BEET_PACKAGE],
     })
+
+    tm.clearSerdePackagesUsed()
+    const serde = tm.mapSerde(type)
+    t.equal(serde, 'beet.coption(beet.u64)', 'option<u64> serde')
+
+    spok(t, Array.from(tm.serdePackagesUsed), {
+      $topic: 'serdePackagesUsed',
+      ...[BEET_PACKAGE],
+    })
   }
+
   t.end()
 })
 
@@ -72,23 +118,42 @@ test('type-mapper: composite types - option<number | bignum>', (t) => {
 test('type-mapper: composite types - vec<number | bignum>', (t) => {
   {
     const tm = new TypeMapper()
-    const ty = tm.map({
+    const type = <IdlType>{
       vec: 'u16',
-    })
+    }
+    const ty = tm.map(type)
 
     t.equal(ty, 'number[]', 'vec<u16>')
     spok(t, Array.from(tm.serdePackagesUsed), {
       $topic: 'serdePackagesUsed',
       ...[],
     })
+
+    tm.clearSerdePackagesUsed()
+    const serde = tm.mapSerde(type)
+    t.equal(serde, 'beet.fixedSizeArray(beet.u16, 1)', 'vec<u16> serde')
+    spok(t, Array.from(tm.serdePackagesUsed), {
+      $topic: 'serdePackagesUsed',
+      ...[BEET_PACKAGE],
+    })
   }
 
   {
     const tm = new TypeMapper()
-    const ty = tm.map({
+    const type = <IdlType>{
       vec: 'u64',
-    })
+    }
+    const ty = tm.map(type)
+
     t.equal(ty, 'beet.bignum[]', 'vec<u64>')
+    spok(t, Array.from(tm.serdePackagesUsed), {
+      $topic: 'serdePackagesUsed',
+      ...[],
+    })
+
+    tm.clearSerdePackagesUsed()
+    const serde = tm.mapSerde(type)
+    t.equal(serde, 'beet.fixedSizeArray(beet.u64, 1)', 'vec<u64> serde')
     spok(t, Array.from(tm.serdePackagesUsed), {
       $topic: 'serdePackagesUsed',
       ...[BEET_PACKAGE],
@@ -102,15 +167,25 @@ test('type-mapper: composite types - vec<number | bignum>', (t) => {
 // -----------------
 test('type-mapper: composite types - user defined', (t) => {
   const tm = new TypeMapper()
-  const ty = tm.map({
+  const type = <IdlType>{
     defined: 'ConfigData',
-  })
+  }
+  const ty = tm.map(type)
 
   t.equal(ty, 'definedTypes.ConfigData')
   spok(t, Array.from(tm.serdePackagesUsed), {
     $topic: 'serdePackagesUsed',
     ...[LOCAL_TYPES_PACKAGE],
   })
+  tm.clearSerdePackagesUsed()
+
+  const serde = tm.mapSerde(type)
+  t.equal(serde, 'definedTypes.ConfigData.struct')
+  spok(t, Array.from(tm.serdePackagesUsed), {
+    $topic: 'serdePackagesUsed',
+    ...[LOCAL_TYPES_PACKAGE],
+  })
+
   t.end()
 })
 
@@ -126,6 +201,14 @@ test('type-mapper: type extensions - publicKey', (t) => {
     $topic: 'serdePackagesUsed',
     ...[SOLANA_WEB3_PACKAGE],
   })
+
+  tm.clearSerdePackagesUsed()
+  const serde = tm.mapSerde('publicKey')
+  t.equal(serde, 'beetSolana.publicKey', 'publicKey serde')
+  spok(t, Array.from(tm.serdePackagesUsed), {
+    $topic: 'serdePackagesUsed',
+    ...[BEET_SOLANA_PACKAGE],
+  })
   t.end()
 })
 
@@ -135,25 +218,48 @@ test('type-mapper: type extensions - publicKey', (t) => {
 test('type-mapper: composite with type extensions - publicKey', (t) => {
   const tm = new TypeMapper()
 
-  const ty = tm.map({
+  const type = <IdlType>{
     option: 'publicKey',
-  })
+  }
+
+  const ty = tm.map(type)
   t.equal(ty, 'beet.COption<web3.PublicKey>', 'option<publicKey>')
   spok(t, Array.from(tm.serdePackagesUsed), {
     $topic: 'serdePackagesUsed',
     ...[SOLANA_WEB3_PACKAGE, BEET_PACKAGE],
+  })
+
+  tm.clearSerdePackagesUsed()
+  const serde = tm.mapSerde(type)
+  t.equal(
+    serde,
+    'beet.coption(beetSolana.publicKey)',
+    'option<publicKey> serde'
+  )
+  spok(t, Array.from(tm.serdePackagesUsed), {
+    $topic: 'serdePackagesUsed',
+    ...[BEET_SOLANA_PACKAGE, BEET_PACKAGE],
   })
   t.end()
 })
 
 test('type-mapper: composite types multilevel - option<option<number>>', (t) => {
   const tm = new TypeMapper()
-  const ty = tm.map({
+  const type = <IdlType>{
     option: {
       option: 'u64',
     },
-  })
+  }
+  const ty = tm.map(type)
   t.equal(ty, 'beet.COption<beet.COption<beet.bignum>>')
+  spok(t, Array.from(tm.serdePackagesUsed), {
+    $topic: 'serdePackagesUsed',
+    ...[BEET_PACKAGE],
+  })
+
+  tm.clearSerdePackagesUsed()
+  const serde = tm.mapSerde(type)
+  t.equal(serde, 'beet.coption(beet.coption(beet.u64))')
   spok(t, Array.from(tm.serdePackagesUsed), {
     $topic: 'serdePackagesUsed',
     ...[BEET_PACKAGE],
@@ -163,32 +269,54 @@ test('type-mapper: composite types multilevel - option<option<number>>', (t) => 
 
 test('type-mapper: composite types multilevel - option<option<publicKey>>', (t) => {
   const tm = new TypeMapper()
-  const ty = tm.map({
+  const type = <IdlType>{
     option: {
       option: 'publicKey',
     },
-  })
+  }
+  const ty = tm.map(type)
   t.equal(ty, 'beet.COption<beet.COption<web3.PublicKey>>')
   spok(t, Array.from(tm.serdePackagesUsed), {
     $topic: 'serdePackagesUsed',
     ...[SOLANA_WEB3_PACKAGE, BEET_PACKAGE],
+  })
+
+  tm.clearSerdePackagesUsed()
+  const serde = tm.mapSerde(type)
+  t.equal(serde, 'beet.coption(beet.coption(beetSolana.publicKey))')
+  spok(t, Array.from(tm.serdePackagesUsed), {
+    $topic: 'serdePackagesUsed',
+    ...[BEET_SOLANA_PACKAGE, BEET_PACKAGE],
   })
   t.end()
 })
 
 test('type-mapper: composite types multilevel - vec<option<ConfigData>>', (t) => {
   const tm = new TypeMapper()
-  const ty = tm.map({
+  const type = <IdlType>{
     vec: {
       option: {
         defined: 'ConfigData',
       },
     },
-  })
+  }
+  const ty = tm.map(type)
   t.equal(ty, 'beet.COption<definedTypes.ConfigData>[]')
   spok(t, Array.from(tm.serdePackagesUsed), {
     $topic: 'serdePackagesUsed',
     ...[LOCAL_TYPES_PACKAGE, BEET_PACKAGE],
   })
+
+  tm.clearSerdePackagesUsed()
+  const serde = tm.mapSerde(type)
+  t.equal(
+    serde,
+    'beet.fixedSizeArray(beet.coption(definedTypes.ConfigData.struct), 1)'
+  )
+  spok(t, Array.from(tm.serdePackagesUsed), {
+    $topic: 'serdePackagesUsed',
+    ...[LOCAL_TYPES_PACKAGE, BEET_PACKAGE],
+  })
+
   t.end()
 })
