20c3a4f00c48eddd784a93acd60771c2a71bd3cd
Thorsten Lorenz <thlorenz@gmx.de>
2022-01-10 15:45:41 -0600

    test: util now verifies code syntactic correctness

--- test/utils/verify-code.ts
+++ test/utils/verify-code.ts
@@ -5,6 +5,20 @@ import path from 'path'
 import { build, BuildResult, Metafile } from 'esbuild'
 import { Test } from 'tape'
 import spok from 'spok'
+import { inspect } from 'util'
+import { ESLint } from 'eslint'
+
+const eslint = new ESLint({
+  overrideConfig: {
+    plugins: ['@typescript-eslint'],
+    parser: '@typescript-eslint/parser',
+    extends: [
+      'eslint:recommended',
+      'plugin:@typescript-eslint/eslint-recommended',
+      'plugin:@typescript-eslint/recommended',
+    ],
+  },
+})
 
 const tmpdir = os.tmpdir()
 
@@ -17,6 +31,10 @@ type AnalyzedCode = {
   errors: BuildResult['errors']
   warnings: BuildResult['warnings']
   imports: Metafile['inputs'][0]['imports']
+}
+
+export function deepLog(obj: any) {
+  console.log(inspect(obj, { depth: 15, colors: true }))
 }
 
 export async function analyzeCode(ts: string) {
@@ -42,15 +60,30 @@ export async function analyzeCode(ts: string) {
   }
 }
 
-export async function verifyImports(
+export const DEFAULT_VERIFY_IMPORTS_OPTS = {
+  expectNoErrors: true,
+  expectNoWarnings: true,
+  logImports: false,
+}
+
+function importsFromCode(code: string) {
+  return code
+    .split('\n')
+    .filter((x) => /^import .+ from/.test(x))
+    .map((x) => x.replace(/;$/, ''))
+}
+
+export function verifyImports(
   t: Test,
   analyzeCode: AnalyzedCode,
   imports: string[],
-  opts: { expectNoErrors: boolean; expectNoWarnings: boolean } = {
-    expectNoErrors: true,
-    expectNoWarnings: true,
-  }
+  opts: Partial<{
+    expectNoErrors: boolean
+    expectNoWarnings: boolean
+    logImports: boolean
+  }> = DEFAULT_VERIFY_IMPORTS_OPTS
 ) {
+  opts = { ...DEFAULT_VERIFY_IMPORTS_OPTS, ...opts }
   if (opts.expectNoErrors) {
     t.equal(analyzeCode.errors.length, 0, 'no errors')
   }
@@ -58,8 +91,35 @@ export async function verifyImports(
     t.equal(analyzeCode.warnings.length, 0, 'no warnings')
   }
 
-  const actual = analyzeCode.imports.map((x) => x.path)
+  const fromCode = importsFromCode(analyzeCode.js)
+  const actual = Array.from(
+    new Set([...analyzeCode.imports.map((x) => x.path), ...fromCode])
+  )
+
   actual.sort()
   imports.sort()
+
+  if (opts.logImports) {
+    console.log({ imports: actual })
+  }
+  t.equal(actual.length, imports.length, 'imports count')
   spok(t, { ...actual, $topic: 'imports' }, imports)
+}
+
+export async function verifySyntacticCorrectness(t: Test, ts: string) {
+  try {
+    const results: ESLint.LintResult[] = await eslint.lintText(ts)
+    for (const res of results) {
+      if (res.errorCount > 0) {
+        deepLog(
+          res.messages.map(
+            (x) => `${x.message} at ${x.line}:${x.column} (${x.nodeType})`
+          )
+        )
+        t.fail(`Found ${res.errorCount} errors via esbuild`)
+      }
+    }
+  } catch (err) {
+    t.error(err)
+  }
 }
