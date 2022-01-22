249ff842f1f884f8d156233e6308962d904fad8c
Thorsten Lorenz <thlorenz@gmx.de>
2022-01-22 10:29:14 -0600

    test: add fanout test using enum

---
+++ test/integration/fanout.ts
@@ -0,0 +1,16 @@
+import { Idl, Solita } from '../../src/solita'
+import test from 'tape'
+import path from 'path'
+import { verifySyntacticCorrectnessForGeneratedDir } from '../utils/verify-code'
+import json from './fixtures/fanout.json'
+
+const outputDir = path.join(__dirname, 'output', 'fanout')
+const generatedSDKDir = path.join(outputDir, 'generated')
+
+test('renders type correct SDK for fanout', async (t) => {
+  const idl = json as Idl
+  idl.metadata = { ...idl.metadata, address: 'fanout program id' }
+  const gen = new Solita(idl, { formatCode: true })
+  await gen.renderAndWriteTo(generatedSDKDir)
+  await verifySyntacticCorrectnessForGeneratedDir(t, generatedSDKDir)
+})
