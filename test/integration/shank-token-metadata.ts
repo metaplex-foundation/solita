b627c27dc3417f65fa1a88ec19fe774a1fa21e88
Thorsten Lorenz <thlorenz@gmx.de>
2022-03-18 11:26:02 -0600

    test: adding fixable specific tests to token-metadata integration test

--- test/integration/shank-token-metadata.ts
+++ test/integration/shank-token-metadata.ts
@@ -1,5 +1,6 @@
 import { Idl, Solita } from '../../src/solita'
 import test from 'tape'
+import { promises as fs } from 'fs'
 import path from 'path'
 import {
   verifySyntacticCorrectnessForGeneratedDir,
@@ -10,10 +11,21 @@ import json from './fixtures/shank_token_metadata.json'
 const outputDir = path.join(__dirname, 'output', 'shank-token-metadata')
 const generatedSDKDir = path.join(outputDir, 'generated')
 
-test('renders type correct SDK for shank_token_metadata', async (t) => {
+test('renders type correct SDK for shank_token_metadata and bubbles fixables', async (t) => {
   const idl = json as Idl
   const gen = new Solita(idl, { formatCode: true })
   await gen.renderAndWriteTo(generatedSDKDir)
   await verifySyntacticCorrectnessForGeneratedDir(t, generatedSDKDir)
   await verifyTopLevelScriptForGeneratedDir(t, generatedSDKDir)
+
+  async function verifyCodeMatches(relPath: string, rx: RegExp) {
+    const fullPath = path.join(generatedSDKDir, relPath)
+    const code = await fs.readFile(fullPath, 'utf8')
+    t.match(code, rx, `Code inside ${relPath} matches ${rx.toString()}`)
+  }
+  await verifyCodeMatches('types/Data.ts', /FixableBeetArgsStruct<\s*Data\s*>/)
+  await verifyCodeMatches(
+    'instructions/CreateMetadataAccount.ts',
+    /FixableBeetArgsStruct<\s*CreateMetadataAccountInstructionArgs/
+  )
 })
