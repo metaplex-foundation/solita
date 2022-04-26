13e88adff2c0142d4a7c025e056bb7cd7a22bb00
Thorsten Lorenz <thlorenz@gmx.de>
2022-04-26 17:52:48 -0500

    ex: update basic_4 example to use latest solita config

---
+++ test/anchor-examples/basic-4/.solitarc.js
@@ -0,0 +1,16 @@
+// @ts-check
+const path = require('path')
+const programDir = path.join(__dirname, 'programs', 'basic-4')
+const idlDir = path.join(__dirname, 'idl')
+const sdkDir = path.join(__dirname, 'src', 'generated')
+const binaryInstallDir = path.join(__dirname, '.crates')
+
+module.exports = {
+  idlGenerator: 'anchor',
+  programName: 'basic_4',
+  programId: 'CwrqeMj2U8tFr1Rhkgwc84tpAsqbt9pTt2a4taoTADPr',
+  idlDir,
+  sdkDir,
+  binaryInstallDir,
+  programDir,
+}
