5e57ed5997e4e4dda1b89fd88d0cd1f72b550784
Thorsten Lorenz <thlorenz@gmx.de>
2022-04-27 11:59:32 -0500

    test: showing empty ix accounts issue

---
+++ test/integration/issue-empty-accounts.ts
@@ -0,0 +1,19 @@
+import { Idl, Solita } from '../../src/solita'
+import test from 'tape'
+import path from 'path'
+import {
+  verifySyntacticCorrectnessForGeneratedDir,
+  verifyTopLevelScriptForGeneratedDir,
+} from '../utils/verify-code'
+import json from './fixtures/issue-empty-accounts.json'
+
+const outputDir = path.join(__dirname, 'output', 'issue-empty-accounts')
+const generatedSDKDir = path.join(outputDir, 'generated')
+
+test('renders type correct SDK for issue-empty-accounts', async (t) => {
+  const idl = json as Idl
+  const gen = new Solita(idl, { formatCode: true })
+  await gen.renderAndWriteTo(generatedSDKDir)
+  await verifySyntacticCorrectnessForGeneratedDir(t, generatedSDKDir)
+  await verifyTopLevelScriptForGeneratedDir(t, generatedSDKDir)
+})
