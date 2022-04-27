14d6767f0b24a36c9598791fec81fb93e6c0de9c
Thorsten Lorenz <thlorenz@gmx.de>
2022-04-27 11:34:58 -0500

    test: adding missing beet import integration test

---
+++ test/integration/issue-missing-beet-import.ts
@@ -0,0 +1,19 @@
+import { Idl, Solita } from '../../src/solita'
+import test from 'tape'
+import path from 'path'
+import {
+  verifySyntacticCorrectnessForGeneratedDir,
+  verifyTopLevelScriptForGeneratedDir,
+} from '../utils/verify-code'
+import json from './fixtures/issue-missing-beet-import.json'
+
+const outputDir = path.join(__dirname, 'output', 'issue-missing-beet-import')
+const generatedSDKDir = path.join(outputDir, 'generated')
+
+test('renders type correct SDK for issue-missing-beet-import', async (t) => {
+  const idl = json as Idl
+  const gen = new Solita(idl, { formatCode: true })
+  await gen.renderAndWriteTo(generatedSDKDir)
+  await verifySyntacticCorrectnessForGeneratedDir(t, generatedSDKDir)
+  await verifyTopLevelScriptForGeneratedDir(t, generatedSDKDir)
+})
