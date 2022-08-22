85c227f21993c7367b688629e99dcffbfb6b8ddf
Thorsten Lorenz <thlorenz@gmx.de>
2022-08-22 12:10:20 -0500

    chore: move fixed enums tests

--- test/integration/feat-mixed-enums.ts
+++ test/integration/feat-mixed-enums.ts
@@ -6,13 +6,13 @@ import {
   verifyTopLevelScriptForGeneratedDir,
   verifyWithTypescriptCompiler,
 } from '../utils/verify-code'
-import json from './fixtures/issue-mixed-enums.json'
+import json from './fixtures/feat-mixed-enums.json'
 import { sync as rmrf } from 'rimraf'
 
-const outputDir = path.join(__dirname, 'output', 'issue-mixed-enums')
+const outputDir = path.join(__dirname, 'output', 'feat-mixed-enums')
 const generatedSDKDir = path.join(outputDir, 'generated')
 
-test('renders type correct SDK for issue-mixed-enums', async (t) => {
+test('renders type correct SDK for feat-mixed-enums', async (t) => {
   rmrf(outputDir)
   const idl = json as Idl
   idl.metadata = {
